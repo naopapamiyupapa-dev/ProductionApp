4da534d6df9f67d1954f4870f1242dbf1d5aa1d8
tomoaki <naopapamiyupapa@gmail.com>
2025-12-27 20:52:31 +0900

    GenbaTool_V1.1.0_Dashboard_MajorUpdate_20251227

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.genba"
         minSdk = 30
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1.0"
+        versionCode = 12
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
